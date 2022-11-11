39b9ee0662d22f65fb81b592fe93290e70b6facd
ALEEF02 <thesmarty2002@gmail.com>
2022-11-10 21:10:41 -0500

    Implement the Welcome Page

--- MavenBack/src/main/java/ppp/staticServe/MainServe.java
+++ MavenBack/src/main/java/ppp/staticServe/MainServe.java
@@ -7,6 +7,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet("") // Disable until future need
 public class MainServe extends HttpServlet {
@@ -15,11 +16,22 @@ public class MainServe extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		
 		try {
-			
-			RequestDispatcher view = request.getRequestDispatcher("/index.html"); // The static HTML file to serve.
-			response.setHeader("Cache-Control", "max-age=86400, public"); // 1 day
-			System.out.println(view.toString());
-			view.forward(request, response);
+			if (request.getSession().getAttribute("new") == null && request.getSession().getAttribute("email") == null) {
+			    // Freshly created.
+				RequestDispatcher view = request.getRequestDispatcher("/home.html"); // The static HTML file to serve.
+				response.setHeader("Cache-Control", "no-store, must-revalidate");
+				response.setHeader("Pragma", "no-cache");
+				response.setHeader("Expires", "0");
+				System.out.println(view.toString());
+				view.forward(request, response);
+			} else {
+			    // Already created.
+				RequestDispatcher view = request.getRequestDispatcher("/index.html"); // The static HTML file to serve.
+				response.setHeader("Cache-Control", "max-age=86400, public"); // 1 day
+				System.out.println(view.toString());
+				view.forward(request, response);
+			}
+			request.getSession().setAttribute("new", false);
 			return;
 			
 		} catch (Exception e) {
